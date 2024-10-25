d5626d513971e3585e902b4c220158d283b409e3
Desai Wang <dw584@cornell.edu>
2024-10-25 15:56:20 +0800

    clickableHistogram working

--- js/clickableHistogramSlider.js
+++ js/clickableHistogramSlider.js
@@ -1,106 +1,121 @@
-export function clickableHistogramSlider(dataAll, svgHist, attribute) {
+/**
+ * Creates a clickable histogram slider using D3.js.
+ * 
+ * @param {Array<Object>} dataAll - The dataset to be visualized.
+ * @param {Object} svg - The SVG element where the histogram will be rendered.
+ * @param {string} attribute - The attribute of the dataset to be used for the histogram.
+ * @param {number} sliderWidth - The width of the histogram slider.
+ * @param {number} sliderHeight - The height of the histogram slider.
+ * @param {function} updateData - The update function after filters are changed.
+* @param {Object} filters - The filters object to be updated.
+ * @param {Array<string>} [colorList=null] - Optional list of colors for the histogram bars, this must match the number of unique values in sorted order.
+ * 
+ * @returns {void}
+ */
+export function clickableHistogramSlider(dataAll, svg, attribute, sliderWidth, sliderHeight, updateData, filters, colorList = null) {
 
   //TODO: change these placeholder colors
   const greyedoutColor = "grey";
-  const selectedColor = "blue";
+  const selectedColor = "black";
 
   const uniqueValues = [...new Set(dataAll.map(d => d[attribute]))].sort((a, b) => a - b);
   console.log("uniqueValues in dataAll", uniqueValues);
 
-  const marginHist = ({ top: 5, right: 20, bottom: 10, left: 10 })
-  const widthHist = 275;
-  const heightHist = 100;
+  const svgHist = svg;
+  const widthHist = sliderWidth;
+  const heightHist = sliderHeight;
+
   let bins = d3.bin().thresholds(uniqueValues).value(d => d[attribute])(dataAll);
   console.log("bins", bins);
   let barPadding = 8;
   //let svgHist = d3.select("#histogram");
 
-  let xScale = d3
-    .scaleLinear()
-    .domain([bins[0].x0, bins[bins.length - 1].x1 + 0.5])
-    .range([marginHist.left, widthHist - marginHist.right])
-
-  let x = d3.scaleBand()
+  let xScale = d3.scaleBand()
     .domain(uniqueValues)
-    .range([marginHist.left, widthHist - marginHist.right + barPadding * 2])
-    .padding(0.3) //TODO: maybe set this instead of multiplying in Padding
+    .range([0, widthHist])
+    .padding(0.2) //TODO: maybe set this instead of multiplying in Padding
 
   let yScale = d3
     .scaleLinear()
     .domain([0, d3.max(bins, d => d.length)])
     .nice()
-    .range([heightHist - marginHist.bottom, marginHist.top])
+    .range([heightHist, 0])
 
   let xAxis = (g) => {
     g
-      .attr("transform", `translate(${0},${heightHist - marginHist.bottom})`)
+      .attr("transform", `translate(${0},${heightHist})`)
       .call(
-        d3.axisBottom(x)
-          .ticks(0)
+        d3.axisBottom(xScale)
           .tickSizeOuter(0)
           .tickFormat(d3.format(".0f"))
       )
   };
+  svgHist.append("g").call(xAxis);
 
-  let ratinglist = []; //used to update ratings
+  //initiate valueList with all unique values (all checked)
+  let valueList = uniqueValues; //used to update ratings
+
+  //save colors for each bin, set clicked all to true
+  bins.forEach((d, i) => {
+    d.color = colorList ? colorList[i] : white
+    d.clicked = true
+  })
 
   svgHist
     .selectAll("rect")
     .data(bins)
     .join("rect")
-    .attr("x", d => xScale(d.x0) + barPadding) // position of each bar on xAxis, width adjustment (bar padding)
+    .attr("x", d => xScale(d.x0)) // position of each bar on xAxis, width adjustment (bar padding)
     .attr("y", d => yScale(d.length))
-    .attr("width", d => d3.max([0, xScale(5) - xScale(4) - barPadding]))
+    .attr("width", xScale.bandwidth())
     .attr("height", d => yScale(0) - yScale(d.length))
     .style("border-radius", "1px")
     .style("outline", "solid")
     .style("outline-width", "thin")
-    .style("outline-color", "greyedoutColor")
-    .style("outline-width", "thin")
-    .attr("fill", "white")
+    .style("outline-color", greyedoutColor)
+    .attr("fill", d => d.color)
     .attr("cursor", "pointer")
     .on('mouseover', function (event, d) {
-      // console.log(event, d)
-      if (!d.clicked) {
-        d3.select(this).attr("fill", "white").style("outline-width", "medium")
-        //d3.select(this).style('outline-color', greyedoutColor);
-      }
-      else {
-        d3.select(this).attr("fill", "#EFE9F8").style("outline-width", "medium")
-        //d3.select(this).style('outline-color', selectedColor);            
-      }
+      d3.select(this).style("outline-width", "medium")
     })
     .on('mouseout', function (event, d) {
       if (!d.clicked) {
-        d3.select(this).attr("fill", "white").style("outline-width", "thin")
-        d3.select(this).style('outline-color', greyedoutColor)
-        //.attr('style', 'outline: thin solid clear;');
+        d3.select(this)
+          .style("outline-width", "thin")
+          .style('outline-color', greyedoutColor)
       }
       else {
-        d3.select(this).attr("fill", "#EFE9F8").style("outline-width", "thin")
-        d3.select(this).style('outline-color', selectedColor);
+        d3.select(this)
+          .style("outline-width", "thin")
+          .style('outline-color', selectedColor);
       }
     })
     .on('click', function (event, d) {
       d.clicked = !d.clicked
 
       if (d.clicked) {
-        d3.select(this).attr("fill", "#EFE9F8")
-        d3.select(this).style('outline-color', selectedColor);
+        d3.select(this)
+          .attr("fill", d => d.color)
+          .style('outline-color', selectedColor);
 
-        ratinglist.push(d.x0);
-        console.log("ratinglist", ratinglist)
-        //updateData();
+        valueList.push(d.x0);
+        console.log("valueList", valueList)
+
+        filters[attribute] = d => valueList.includes(d[attribute]);
+        updateData()
       } else {
-        d3.select(this).attr("fill", "white")
-        d3.select(this).style('outline-color', greyedoutColor)
+        d3.select(this)
+          .attr("fill", "white")
+          .style('outline-color', greyedoutColor)
 
-        ratinglist = ratinglist.filter(rating => rating != d.x0)
-        console.log("ratinglist", ratinglist)
-        //updateData();
+        valueList = valueList.filter(rating => rating != d.x0)
+        console.log("valueList", valueList)
+        filters[attribute] = d => valueList.includes(d[attribute]);
+        updateData();
       }
     });
 
-  svgHist.append("g").call(xAxis);
+
+
 }
 
