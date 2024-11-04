31f21b2be2425a6b6cb7cdb39854d8cbb20b5ed6
Desai Wang <dw584@cornell.edu>
2024-11-04 09:01:34 +0800

    implemented any/none for searchBars

--- js/searchBar.js
+++ js/searchBar.js
@@ -8,12 +8,26 @@ class SearchBar {
   }
 
   initialize() {
-    console.log("this.input", this.input)
     this.input.on("input", (event) => {
       const query = event.target.value;
-      this.filters[this.attribute] = d =>
-        d[this.attribute] &&
-        d[this.attribute].toLowerCase().includes(query.toLowerCase());
+      if (query === "") {
+        this.filters[this.attribute] = d =>
+          true;
+      }
+      else if (query === "any") {
+        this.filters[this.attribute] = d =>
+          d[this.attribute];
+      }
+      else if (query === "none") {
+        this.filters[this.attribute] = d =>
+          !d[this.attribute];
+      }
+      else {
+        this.filters[this.attribute] = d =>
+          d[this.attribute] &&
+          d[this.attribute].toLowerCase().includes(query.toLowerCase());
+      }
+
       this.updateData();
     });
   }
