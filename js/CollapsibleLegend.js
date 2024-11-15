ce40813962d99c68a01a3ad4622dae95d79c3474
Desai Wang <dw584@cornell.edu>
2024-11-16 00:50:49 +0800

    testing legend

--- js/CollapsibleLegend.js
+++ js/CollapsibleLegend.js
@@ -1,13 +1,18 @@
-import { Legend } from './legend.js';
+import { Legend } from "./legend.js";
 class CollapsibleLegend {
-  constructor(div, initialcolorAttribute, initialLegendNode, salaryScale, options = {}) {
-
+  constructor(
+    div,
+    initialcolorAttribute,
+    initialLegendNode,
+    salaryScale,
+    options = {}
+  ) {
     // Default options
     const defaultOptions = {
       colorLegendWidth: 250,
       colorLegendHeight: 40,
       titleFontSize: "0.8rem",
-      tickFontSize: "0.6rem"
+      tickFontSize: "0.6rem",
     };
 
     // Merge user-provided options with defaults
@@ -20,78 +25,99 @@ class CollapsibleLegend {
   }
 
   initialize() {
-
-    let button = this.div.append("div").
-      append("button").attr("class", "collapse title");
-    this.chevron = button.append("i")
+    let button = this.div
+      .append("div")
+      .append("button")
+      .attr("class", "collapse title");
+    this.chevron = button
+      .append("i")
       .attr("class", "bx bx-chevron-right")
-      .style("rotate", "90deg")
-      ;
-    button.append("span").text("Legend")
+      .style("rotate", "90deg");
+    button.append("span").text("Legend");
 
     //this.input stores everthing that can be toggled
-    this.input = this.div.append("div").attr("id", "wrapper").style("padding-left", "1.5rem");
-    this.sizeLegend = this.input.append("div").attr("id", "sizeLegend").style("padding", "0.5rem 0rem");
-    this.colorLegend = this.input.append("div").attr("id", "colorLegend").style("padding", "0rem 0rem 0.5rem");
+    this.input = this.div
+      .append("div")
+      .attr("id", "wrapper")
+      .style("padding-left", "1.5rem");
+    this.sizeLegend = this.input
+      .append("div")
+      .attr("id", "sizeLegend")
+      .style("padding", "0.5rem 0rem");
+    this.colorLegend = this.input
+      .append("div")
+      .attr("id", "colorLegend")
+      .style("padding", "0rem 0rem 0.5rem");
     //add control to button
     this.collapsed = false;
     button.on("click", () => {
       this.collapsed = !this.collapsed;
       this.onCollapsedChange();
     });
-
   }
 
   setupSalaryLegend(salaryScale) {
-
-    this.sizeLegend.append("div").attr("id", "colorLegendAttribute")
+    this.sizeLegend
+      .append("div")
+      .attr("id", "colorLegendAttribute")
       // .style("margin-bottom", "0rem")
       .style("font-size", this.options.titleFontSize)
       .text("salary (circle size)");
 
-    let salaryPoints = [50000, 100000, 150000, 200000, 250000]
+    let salaryPoints = [50000, 100000, 150000, 200000, 250000];
 
-    let circles = this.sizeLegend.append("svg")
+    let circles = this.sizeLegend
+      .append("svg")
       .attr("width", this.options.colorLegendWidth)
       .attr("height", this.options.colorLegendHeight)
       .selectAll()
       .data(salaryPoints)
       .join("g")
-      .style("transform", (d, i) => `translate(${35 + i * 50 - 0.5 * salaryScale(d)}px, 18px)`)
+      .style(
+        "transform",
+        (d, i) => `translate(${35 + i * 50 - 0.5 * salaryScale(d)}px, 18px)`
+      );
 
     circles
       .append("circle")
-      .attr("r", d => salaryScale(d))
+      .attr("r", (d) => salaryScale(d))
       // .attr("y", d => -0.5 * salaryScale(d))
       .style("transform", (d, i) => `translate(0px,${-1 * salaryScale(d)}px)`)
       .attr("stroke", "#414651") //grey-700
       .attr("stroke-width", 0.8)
-      .attr("fill", "none")
+      .attr("fill", "none");
 
-    circles.append("text")
+    circles
+      .append("text")
       .style("font-size", this.options.tickFontSize)
       .attr("y", 12)
-      .text(d => d3.format("$.2s")(d))
-      .each(function (d) { // Center text based on its width
+      .text((d) => d3.format("$.2s")(d))
+      .each(function (d) {
+        // Center text based on its width
         const textWidth = this.getBBox().width;
-        d3.select(this).attr("x", - textWidth / 2);
-      })
+        d3.select(this).attr("x", -textWidth / 2);
+      });
   }
-  updateColorScale(colorAttribute, legendNode) {
 
+  updateColorScale(colorAttribute, legendNode) {
     // Remove existing child nodes
     while (this.colorLegend.node().firstChild) {
       this.colorLegend.node().removeChild(this.colorLegend.node().firstChild);
     }
 
-    this.colorLegend.append("div").attr("id", "colorLegendAttribute")
+    this.colorLegend
+      .append("div")
+      .attr("id", "colorLegendAttribute")
       .style("margin-bottom", "0.5rem")
       .style("font-size", this.options.titleFontSize)
       .text(`${colorAttribute.toLowerCase()} (circle color)`);
 
     // Append the new legend node
-    this.colorLegend.append("div").style("margin-left", "0.5rem")
-    .node().appendChild(legendNode);
+    this.colorLegend
+      .append("div")
+      .style("margin-left", "0.5rem")
+      .node()
+      .appendChild(legendNode);
   }
 
   changeCollapsed(bool) {
@@ -100,9 +126,9 @@ class CollapsibleLegend {
   }
 
   onCollapsedChange() {
-    this.chevron.transition()
-      .style("rotate", this.collapsed ? "0deg" : "90deg")
-
+    this.chevron
+      .transition()
+      .style("rotate", this.collapsed ? "0deg" : "90deg");
 
     if (this.collapsed) {
       this.input
@@ -115,7 +141,7 @@ class CollapsibleLegend {
         .style("display", "block")
         .transition()
         .style("opacity", 1)
-        .style("visibility", "visible")
+        .style("visibility", "visible");
     }
   }
 }
--- js/hexbinMap.js
+++ js/hexbinMap.js
@@ -1,6 +1,140 @@
 import { geoAlbersUsaPr } from "./geoAlbersUsaPr.js";
+import { Legend } from "./legend.js";
 
-export async function hexbinMap(
+class HexbinMap {
+  constructor(divID, stateMesh, colorScales) {
+    this.divID = divID;
+    this.stateMesh = stateMesh;
+    this.colorScales = colorScales;
+    this.initialize();
+  }
+
+  initialize() {
+    //set up for map
+    this.widthMap = 928;
+    this.heightMap = 581;
+
+    // Create the container SVG for map
+    this.svgMap = d3
+      .select(`div#${this.divID}`)
+      .append("svg")
+      .attr("viewBox", [0, 0, this.widthMap, this.heightMap]);
+    //.attr("style", "max-width: 100%; height: auto;");
+
+    this.mapArea = this.svgMap.append("g");
+
+    this.projection = geoAlbersUsaPr()
+      .scale((4 / 3) * this.widthMap)
+      .translate([this.widthMap / 2, this.heightMap / 2]);
+
+    // Append the state mesh.
+    this.mapArea
+      .append("path")
+      .datum(this.stateMesh)
+      .attr("fill", "none")
+      .attr("stroke", "#777")
+      .attr("stroke-width", 0.5)
+      .attr("stroke-linejoin", "round")
+      .attr("d", d3.geoPath(this.projection));
+
+    //set up the layer used for hexBin rendering
+    this.hexBinLayer = this.mapArea.append("g");
+    this.hexBinColorScale = this.mapArea
+      .append("g")
+      .attr("id", "hexBinColorScale");
+  }
+
+  render(data, categoryColor, categoryRadius) {
+    console.log("about to render hexbin map", data);
+    //Hexbin map
+    const hexbin = d3
+      .hexbin()
+      .extent([
+        [0, 0],
+        [this.widthMap, this.heightMap],
+      ])
+      .radius(10)
+      .x((d) => {
+        if (d.xy) {
+          return d.xy[0];
+        }
+        //else {console.log("error: d is", d);}
+      })
+      .y((d) => d.xy[1]);
+
+    const bins = hexbin(
+      //Guam unfortunately is not a part of the geo albers projection, so removing it from map visualization
+      data
+        .filter((d) => d.Location !== "Barrigada, GU, US")
+        .map((d) => ({
+          xy: this.projection([d.Longitude, d.Latitude]),
+          Salary: d.Salary,
+          Satisfaction: d["Job Satisfaction"],
+          Licensed: d.Licensed,
+        }))
+    )
+      .map((d) => ((d.salary = d3.median(d, (d) => d.Salary)), d))
+      .map(
+        (d) => ((d["Job Satisfaction"] = d3.mean(d, (d) => d.Satisfaction)), d)
+      )
+      .map(
+        (d) => (
+          (d.percentageLicensed =
+            d.filter((d) => d.Licensed === "Yes").length / d.length),
+          d
+        )
+      );
+
+    // Create the color and radius scales.
+    const BuOrColorInterpolator = d3.piecewise(d3.interpolateRgb, [
+      "#0a1869",
+      "#a9c9e7",
+      "#ffe3a8",
+      "#fc6f03",
+    ]);
+    const color = d3.scaleSequential(
+      d3.extent(bins, (d) => d[categoryColor]),
+      BuOrColorInterpolator
+    );
+
+    this.hexBinColorScale.selectAll("*").remove();
+    this.hexBinColorScale.node().append(Legend(color));
+
+    const radius = d3.scaleSqrt(
+      d3.extent(bins, (d) => d[categoryRadius]),
+      [hexbin.radius() * 0.3, hexbin.radius() * 3]
+    );
+
+    // Append the hexagons
+    this.hexBinLayer.selectAll("path").remove();
+    this.hexBinLayer
+      .selectAll("path")
+      .data(bins)
+      .join("path")
+      .attr("class", "hexBin")
+      .attr("transform", (d) => `translate(${d.x},${d.y})`)
+      .attr("d", (d, i) => {
+        // if (d.x == 857.3651497465942 & d.y == 165) {
+        //   console.log("d is", d, "d[categoryRadius]: ", d[categoryRadius], "radius: ", radius(d[categoryRadius]))
+        // }
+        return hexbin.hexagon(radius(d[categoryRadius]));
+      })
+      .attr("fill", (d) => color(d[categoryColor]))
+      .attr("stroke", (d) => d3.lab(color(d[categoryColor])).darker())
+      .attr("opacity", 0.8)
+      .append("title")
+      .text(
+        (d) =>
+          `${d.length.toLocaleString()} survey responses\n${d3.format(".2f")(
+            d["Job Satisfaction"]
+          )} mean satisfaction\n${d3.format("$.2s")(d.salary)} median salary\n${
+            d.percentageLicensed
+          } licensed`
+      );
+  }
+}
+
+function hexbinMap(
   projection,
   data,
   categoryColor,
@@ -9,84 +143,16 @@ export async function hexbinMap(
   widthMap,
   heightMap
 ) {
-  //Hexbin map
-  const hexbin = d3
-    .hexbin()
-    .extent([
-      [0, 0],
-      [widthMap, heightMap],
-    ])
-    .radius(10)
-    .x((d) => {
-      if (d.xy) {
-        return d.xy[0];
-      } else {
-        console.log("error: d is", d);
-      }
-    })
-    .y((d) => d.xy[1]);
-
-  const bins = hexbin(
-    //Guam unfortunately is not a part of the geo albers projection, so removing it from map visualization
-    data
-      .filter((d) => d.Location !== "Barrigada, GU, US")
-      .map((d) => ({
-        xy: projection([d.Longitude, d.Latitude]),
-        Salary: d.Salary,
-        Satisfaction: d["Job Satisfaction"],
-        Location: d["Location"],
-      }))
-  )
-    .map((d) => ((d.salary = d3.median(d, (d) => d.Salary)), d))
-    .map((d) => ((d.satisfaction = d3.mean(d, (d) => d.Satisfaction)), d));
-
   //console.log("bins", bins)
-
-  // Create the color and radius scales.
-  const color = d3.scaleSequential(
-    d3.extent(bins, (d) => d[categoryColor]),
-    d3.interpolateSpectral
-  );
-  const radius = d3.scaleSqrt(
-    d3.extent(bins, (d) => d[categoryRadius]),
-    [hexbin.radius() * 0.3, hexbin.radius() * 3]
-  );
   //d3.extent(bins, d => d[categoryRadius])
-
-  // Append the hexagons.
-  hexBinLayer.selectAll("path").remove();
-  hexBinLayer
-    .selectAll("path")
-    .data(bins)
-    .join("path")
-    .attr("class", "hexBin")
-    .attr("transform", (d) => `translate(${d.x},${d.y})`)
-    .attr("d", (d, i) => {
-      // if (d.x == 857.3651497465942 & d.y == 165) {
-      //   console.log("d is", d, "d[categoryRadius]: ", d[categoryRadius], "radius: ", radius(d[categoryRadius]))
-      // }
-      return hexbin.hexagon(radius(d[categoryRadius]));
-    })
-    .attr("fill", (d) => color(d[categoryColor]))
-    .attr("stroke", (d) => d3.lab(color(d[categoryColor])).darker())
-    .attr("opacity", 0.8)
-    .append("title")
-    .text(
-      (d) =>
-        `${d.length.toLocaleString()} survey responses\n${d3.format(".2f")(
-          d.satisfaction
-        )} mean satisfaction\n${d3.format("$.2s")(d.salary)} median salary`
-    );
-
-  hexBinLayer.selectAll("circle").remove();
-  hexBinLayer
-    .append("circle")
-    .attr("cx", 857.3651)
-    .attr("cy", 165)
-    .attr("r", 2)
-    .attr("fill", "black");
+  // hexBinLayer.selectAll("circle").remove();
+  // hexBinLayer
+  //   .append("circle")
+  //   .attr("cx", 857.3651)
+  //   .attr("cy", 165)
+  //   .attr("r", 2)
+  //   .attr("fill", "black");
   //zooming functions, uncommented out for now
-
   // var zoom = d3.zoom()
   //   .scaleExtent([1, 15])
   //   .translateExtent([
@@ -94,25 +160,22 @@ export async function hexbinMap(
   //     [widthMap + 50, heightMap + 50]
   //   ]) // to lock to edges
   //   .on("zoom", mapZoomed);
-
   // mapArea.call(zoom);
   // //manually call zoom interaction to activate any code that's in zoomed()
   // mapArea.call(zoom.transform, d3.zoomIdentity);
-
   // function mapZoomed({
   //   transform
   // }) {
   //   // Transform the group object to reflect the zoom action
   //   mapArea.attr("transform", transform.toString());
-
   //   // Divide by scale to make sure strokes remain a consistent width during zooming
   //   // mapArea.select(".state-outline")
   //   //   .style("stroke-width", 2 / transform.k);
   //   // mapArea.select(".county-outline")
   //   //   .style("stroke-width", 1 / transform.k);
-
   //   // mapArea.selectAll("circle")
   //   //   .style("r", d => radius(d.length) / Math.sqrt(transform.k))
-
   // }
 }
+
+export default HexbinMap;
